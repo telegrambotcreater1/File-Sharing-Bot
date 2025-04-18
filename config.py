6ad6400a2780dac8335b0cd42a4b7864d4e4ab31
Exam Panel <133669744+telegrambotcreater1@users.noreply.github.com>
2025-04-18 21:36:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5732813591"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://filmpublisher01:XGLAbXMcIyCce6hq@filestorebot.htkmnpu.mongodb.net/?retryWrites=true&w=majority&appName=filestorebot")
+DB_NAME = os.environ.get("DATABASE_NAME", "filestorebot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
